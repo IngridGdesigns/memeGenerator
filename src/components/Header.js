ed523ad3ced40862b2f85dd8009f9ede2b78dfe2
IngridGdesigns <garciai@protonmail.com>
2023-12-18 22:13:54 -0800

    changed image src and updated alt

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./images/troll-face.png" alt="meme face"
+                src={`${process.env.PUBLIC_URL}/images/cat-face.png`} alt="cat selfie face"
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
